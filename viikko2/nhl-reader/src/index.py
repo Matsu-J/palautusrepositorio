113742641f60b6a357c05bd57b29b2132d4677bc
Matias Jerkkola <matsu.jerkkola@gmail.com>
2025-11-07 14:49:42 +0200

    Added ability to print players by nationality

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,22 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    players = []
+    nationality = "FIN"
+
+    for player_dict in response:
+        if player_dict["nationality"] == nationality:
+            player = Player(player_dict)
+            players.append(player)
+
+    print(f"Players from {nationality}:")
+
+    for player in players:
+        print(player)
 
 if __name__ == "__main__":
     main()
