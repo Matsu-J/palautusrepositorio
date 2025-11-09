0eaa6b36f5b18c17090505065c3da38f8b5dfe27
Matias Jerkkola <matsu.jerkkola@gmail.com>
2025-11-09 19:24:32 +0200

    Refactored code to do with player statistics

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,19 +1,10 @@
-import requests
-from player import Player
+from statistics_service import PlayerReader, PlayerStats
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
-
-    players = []
-    nationality = "FIN"
-
-    for player_dict in response:
-        if player_dict["nationality"] == nationality:
-            player = Player(player_dict)
-            players.append(player)
-
-    print(f"Players from {nationality}:")
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     for player in sorted(players, key=lambda player: player.points, reverse=True):
         print(player)
