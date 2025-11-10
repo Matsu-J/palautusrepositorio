126f8dfdac71c074df30eb30a410f7a383365f39
Matias Jerkkola <matsu.jerkkola@gmail.com>
2025-11-10 05:02:51 +0200

    Added inputs for season and nationality and made outputs fancier

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,22 +1,61 @@
-import requests
-from player import Player
+from statistics_service import PlayerReader, PlayerStats
+from rich import print as rprint
+from rich.table import Table, Column
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    response = requests.get(url).json()
+    print()
+    
+    while True:
+        rprint(f"Season [magenta][2018-19, 2019-20, 2020-21, 2021-22, 2022-23, 2023-24, 2024-25] [blue](2024-25)\n[white]Enter 0 to quit")
+        season = input()
+        seasons = ["2018-19", "2019-20", "2020-21", "2021-22", "2022-23", "2023-24", "2024-25"]
+        if season == "0":
+            return
+        elif season == "":
+            season = "2024-25"
+        if str(season) in seasons:
+            url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
+            reader = PlayerReader(url)
+            stats = PlayerStats(reader)
+            break
+        else:
+            print("No information found")
+            continue
+    
+    while True:
+        rprint("Nationality [magenta][USA, FIN, CAN, SWE, CZE, RUS, SLO, FRA, GBR, SVK, DEN, NED, AUT, BLR, GER, SUI, NOR, UZB, LAT, AUS]\n[white]Enter 0 to quit")
+        nationality = input()
+        nationality = nationality.upper()
+        if nationality == "0":
+            return
+        try:
+            players = stats.top_scorers_by_nationality(nationality)
+            if len(players) <= 0:
+                print("No information found")
+                continue
+        except:
+            print("No information found")
+            continue
+        
+        print()
+        stats_table = Table(
+            "Name",
+            "Team(s)",
+            "Goals",
+            "Assists",
+            "Points",
+            title=f"Season {season} players from {nationality}",
+        )
 
-    players = []
-    nationality = "FIN"
-
-    for player_dict in response:
-        if player_dict["nationality"] == nationality:
-            player = Player(player_dict)
-            players.append(player)
-
-    print(f"Players from {nationality}:")
-
-    for player in sorted(players, key=lambda player: player.points, reverse=True):
-        print(player)
+        for player in sorted(players, key=lambda player: player.points, reverse=True):
+            stats_table.add_row(f"[blue]{player.name}", 
+                                f"[magenta]{player.team}", 
+                                f"[green]{player.goals}", 
+                                f"[green]{player.assists}", 
+                                f"[green]{player.points}")
+        
+        rprint(stats_table)
+        print()
 
 if __name__ == "__main__":
     main()
---
+++ viikko2/nhl-reader/src/statistics_service.py
@@ -0,0 +1,20 @@
+import requests
+from player import Player
+
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+        self.response = requests.get(url).json()
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.reader = reader
+    
+    def top_scorers_by_nationality(self, nationality):
+        players = []
+        for player_dict in self.reader.response:
+            if player_dict["nationality"] == nationality:
+                player = Player(player_dict)
+                players.append(player)
+        return players 
